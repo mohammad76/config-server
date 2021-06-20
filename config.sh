5ee784ec94743df47d6795885711d4748a7d1504
mohammad <mohammad@mohammads-MacBook-Pro.local>
2021-06-20 23:16:22 +0430

    update

--- config.sh
+++ config.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 host_ip=$(hostname -i)
 
-echo "updating os ..."
+# Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+echo "${RED}updating os ..."
 apt update -y && upgrade -y
 
-echo "disable ipv6 ..."
+echo "${RED}disable ipv6 ..."
 sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 sysctl -p
 
-echo "install useful packages ...."
+echo "${RED}install useful packages ...."
 apt install -y rkhunter supervisor net-tools htop build-essential iftop clamav clamav-daemon dnsutils dnsutils dsniff grepcidr software-properties-common
 
-echo "install python 3.9 ...."
+echo "${RED}install python 3.9 ...."
 add-apt-repository ppa:deadsnakes/ppa
 apt update && apt install -y python3.9 python-pip
 update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1
@@ -21,11 +27,12 @@ update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
 update-alternatives --install /usr/bin/python python /usr/bin/python2.7 3
 update-alternatives --config python
 
+echo "${RED}install nodejs 14 ...."
 curl -sL https://deb.nodesource.com/setup_14.x | -E bash -
 apt-get install -y nodejs
 npm install -g npm@latest
 
-echo "install ddos deflate app ...."
+echo "${RED}install ddos deflate app ...."
 wget https://github.com/jgmdev/ddos-deflate/archive/master.zip -O ddos.zip
 unzip ddos.zip
 ./ddos-deflate-master/install.sh
@@ -33,7 +40,7 @@ rm /etc/ddos/ignore.ip.list
 cp ./ignore.ip.list /etc/ddos/ignore.ip.list
 echo "$host_ip/32" >>/etc/ddos/ignore.ip.list
 
-echo "install docker ...."
+echo "${RED}install docker ...."
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
@@ -50,11 +57,11 @@ cat >/etc/docker/daemon.json <<EOF
 EOF
 systemctl restart docker
 
-echo "disable systemd resolved ..."
+echo "${RED}disable systemd resolved ..."
 systemctl disable systemd-resolved.service
 systemctl stop systemd-resolved
 
-echo "add shecan dns ..."
+echo "${RED}add shecan dns ..."
 rm /etc/resolv.conf
 cat >/etc/resolv.conf <<EOF
 nameserver 178.22.122.100
