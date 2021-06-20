aa11586454e6c1453563212e6ac7e69d2f947f78
mohammad <mohammad@mohammads-MacBook-Pro.local>
2021-06-20 22:55:50 +0430

    update

--- config.sh
+++ config.sh
@@ -24,12 +24,7 @@ echo "$host_ip/32" >> /etc/ddos/ignore.ip.list
 echo "install docker ...."
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get update
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
