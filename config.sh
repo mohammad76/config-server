c74d825e2b59a9858f3e47f84924b0e299241644
mohammad <mohammad@mohammads-MacBook-Pro.local>
2021-06-20 23:20:54 +0430

    update

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-echo "${RED}updating os ..."
+echo -e "${RED}updating os ..."
 apt update -y && upgrade -y
 
 echo "${RED}disable ipv6 ..."
