058abdf487f20963f8342dc3c0682f9ba44a1b30
mohammad <mohammad@mohammads-MacBook-Pro.local>
2021-06-20 23:02:34 +0430

    update

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-host_ip=$(hostname  -i)
+host_ip=$(hostname -i)
 
 echo "updating os ..."
 apt update -y && upgrade -y
@@ -11,7 +11,15 @@ sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 sysctl -p
 
 echo "install useful packages ...."
-apt install -y rkhunter net-tools htop iftop clamav clamav-daemon dnsutils dnsutils dsniff grepcidr
+apt install -y rkhunter net-tools htop iftop clamav clamav-daemon dnsutils dnsutils dsniff grepcidr software-properties-common
+
+echo "install python 3.9 ...."
+add-apt-repository ppa:deadsnakes/ppa
+apt update && apt install python3.9
+update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1
+update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
+update-alternatives --install /usr/bin/python python /usr/bin/python2.7 3
+update-alternatives --config python
 
 echo "install ddos deflate app ...."
 wget https://github.com/jgmdev/ddos-deflate/archive/master.zip -O ddos.zip
@@ -19,7 +27,7 @@ unzip ddos.zip
 ./ddos-deflate-master/install.sh
 rm /etc/ddos/ignore.ip.list
 cp ./ignore.ip.list /etc/ddos/ignore.ip.list
-echo "$host_ip/32" >> /etc/ddos/ignore.ip.list
+echo "$host_ip/32" >>/etc/ddos/ignore.ip.list
 
 echo "install docker ...."
 apt-get remove docker docker-engine docker.io containerd runc
@@ -28,10 +36,10 @@ apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
-cat > /etc/docker/daemon.json <<EOF
+cat >/etc/docker/daemon.json <<EOF
 {
   "registry-mirrors": ["https://dockerhub.ir"]
 }
@@ -42,10 +50,9 @@ echo "disable systemd resolved ..."
 systemctl disable systemd-resolved.service
 systemctl stop systemd-resolved
 
-
 echo "add shecan dns ..."
 rm /etc/resolv.conf
-cat > /etc/resolv.conf <<EOF
+cat >/etc/resolv.conf <<EOF
 nameserver 178.22.122.100
 nameserver 185.51.200.2
 EOF
