0a88ebfe4ec18d81f2e411dc6a6edf349c1c60e6
mohammad <mohammad@mohammads-MacBook-Pro.local>
2021-06-21 00:15:34 +0430

    update

--- config.sh
+++ config.sh
@@ -65,6 +65,7 @@ apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+rm /usr/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 cat >/etc/docker/daemon.json <<EOF
