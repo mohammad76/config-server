14dfe70b2950ea3fe92e6ff6ed0b3edd70367466
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2023-05-30 22:41:18 +0330

    Update config.sh

--- config.sh
+++ config.sh
@@ -56,6 +56,7 @@ echo -e "${GREEN}install docker ....${NC}"
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+rm -rf /usr/share/keyrings/docker-archive-keyring.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
