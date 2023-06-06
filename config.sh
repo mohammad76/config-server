502d81a70e689c26b76fd7bbee53d85f8f25bff6
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2023-06-06 05:26:22 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -48,7 +48,7 @@ add-apt-repository ppa:deadsnakes/ppa --yes
 apt update && apt install -y python3.9 python3-pip
 cd /usr/lib/python3/dist-packages
 cp apt_pkg.cpython-38-x86_64-linux-gnu.so apt_pkg.so
-
+cd
 update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
 update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
