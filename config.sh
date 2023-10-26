8e050f4474b7a034331a646ab987e3022bd3c8da
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2023-10-26 20:05:06 +0330

    Update config.sh

--- config.sh
+++ config.sh
@@ -95,10 +95,10 @@ echo \
 apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-rm /usr/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#chmod +x /usr/local/bin/docker-compose
+#rm /usr/bin/docker-compose
+# ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 if [ $IR_SERVER = "y" ]; then
 bash -c 'cat > /etc/docker/daemon.json <<EOF
