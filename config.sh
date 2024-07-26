b2d001edfc7224fd2808ccdf7512dc15112fe121
Mohammad Abdi <mohammad_abdi76@yahoo.com>
2024-07-26 13:28:19 +0330

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-read -p "Enter Server IP: " NODE_IP
-read -p "is an Iran Server?[y/n] " IR_SERVER
 
 # Color
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
+
+SERVER_IP=$(hostname -I | awk '{print $1}')
+IP_CHECK_URL="https://api.country.is/$SERVER_IP"
+CHECK_IP=$(curl -s "$IP_CHECK_URL")
+if echo "$CHECK_IP" | grep -q "\"error\""; then
+  echo -e "${RED} Error! IP address not found ${NC}"
+  exit 1
+fi
+
+COUNTRY=$(echo "$CHECK_IP" | grep -o -P '"country":"\K[^"]+' | tr -d \")
+export COUNTRY
+
+echo -e "${GREEN}Server IP: ${SERVER_IP} ${NC}"
+echo -e "${GREEN}Server Country: ${COUNTRY} ${NC}"
+
 
 echo -e "${GREEN}set Tehran Timezone ...${NC}"
 TZ=Asia/Tehran
@@ -15,21 +28,6 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 echo -e "${GREEN}disable systemd resolved ...${NC}"
 systemctl disable systemd-resolved.service
 systemctl stop systemd-resolved
-
-if [ $IR_SERVER = "y" ]; then
-    echo -e "${GREEN}add proxy dns ...${NC}"
-    rm /etc/resolv.conf
-cat >/etc/resolv.conf <<EOF
-options timeout:1
-nameserver 178.22.122.100
-nameserver 185.51.200.2
-EOF
-
-
-echo -e "${GREEN}change server repo ...${NC}"
-sed -i 's/archive.ubuntu.com/mirror.arvancloud.ir/g' /etc/apt/sources.list
-sed -i 's/ir.mirror.arvancloud.ir/mirror.arvancloud.ir/g' /etc/apt/sources.list
-sed -i 's/us.mirror.arvancloud.ir/mirror.arvancloud.ir/g' /etc/apt/sources.list
 
 else
     echo -e "${GREEN}add base dns ...${NC}"
@@ -56,7 +54,7 @@ iptables-persistent iptables-persistent/autosave_v6 boolean true
 EOF
 
 echo -e "${GREEN}install useful packages ....${NC}"
-DEBIAN_FRONTEND=noninteractive apt install -y rkhunter supervisor net-tools htop fail2ban wget zip nmap git letsencrypt build-essential iftop dnsutils dsniff grepcidr iotop rsync atop software-properties-common
+DEBIAN_FRONTEND=noninteractive apt install -y jq iptables-persistent sqlite3 pigz default-mysql-client rkhunter supervisor btop net-tools htop fail2ban wget zip nmap git letsencrypt build-essential iftop dnsutils dsniff grepcidr iotop rsync atop software-properties-common
 git config --global credential.helper store
 
 echo -e "${GREEN}install python lib ....${NC}"
@@ -74,41 +72,42 @@ curl https://public-chabok.s3.ir-thr-at1.arvanstorage.com/minio-mc-new \
 chmod +x /usr/local/bin/mc
 
 echo -e "${GREEN}install docker ....${NC}"
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-apt-get update
-apt-get install -y ca-certificates curl gnupg
+if [ $os_version = "22" ]; then
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg; done
+elif [ $os_version = "24" ]; then
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg; done
+elif [ $os_version = "20" ]; then
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 containerd runc; do apt-get remove $pkg; done
+fi
+apt-get update -y
+DEBIAN_FRONTEND=noninteractive apt install -y ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-
-UBUNTU_VERSION=$(lsb_release -c)
-UBUNTU_VERSION=${UBUNTU_VERSION#*:}
-if [ $UBUNTU_VERSION = "focal" ]; then
-
-    echo "install focal"
-    VERSION_STRING=5:25.0.3-1~ubuntu.20.04~focal
-    sudo apt-get install -y docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
-
-elif [ $UBUNTU_VERSION = "jammy" ]; then
-    echo "install jammy"
-    VERSION_STRING=5:25.0.3-1~ubuntu.22.04~jammy
-    sudo apt-get install -y docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
-
+if [ $os_version = "22" ]; then
+    VERSION_STRING=5:26.1.4-1~ubuntu.22.04~jammy
+    DEBIAN_FRONTEND=noninteractive apt install -y docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+elif [ $os_version = "24" ]; then
+    VERSION_STRING=5:26.1.4-1~ubuntu.24.04~noble
+    DEBIAN_FRONTEND=noninteractive apt install -y docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+elif [ $os_version = "20" ]; then
+    VERSION_STRING=5:26.1.4-1~ubuntu.20.04~focal
+    DEBIAN_FRONTEND=noninteractive apt install -y docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
 else
-
-    echo "not proper version, please check your ubuntu version first."
-
+    echo -e "${RED} not proper version, please check your ubuntu version first.${NC}"
+    exit 1
 fi
 apt-mark hold docker-ce docker-ce-cli
+apt purge postfix -y
 
-if [ $IR_SERVER = "y" ]; then
+service docker start
+
+if [  $COUNTRY = "IR"  ]; then
 bash -c 'cat > /etc/docker/daemon.json <<EOF
 {
   "insecure-registries" : ["https://docker.chabokan.net"],
